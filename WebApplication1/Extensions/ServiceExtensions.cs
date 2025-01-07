f2ece77850c565385f428645b6041b98821a3ca3
Mohammed Ali <102896991+mohammedAliFish@users.noreply.github.com>
2025-01-07 15:53:04 +0300

    Add project files.

---
+++ WebApplication1/Extensions/ServiceExtensions.cs
@@ -0,0 +1,29 @@
+﻿
+
+using Contracts.Interface;
+using LoggerService;
+
+namespace WebApplication1.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services)=>
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+
+    }
+}
+
---
+++ WebApplication1/Program.cs
@@ -0,0 +1,81 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
+using WebApplication1.Extensions;
+var builder = WebApplication.CreateBuilder(args);
+
+LogManager.LoadConfiguration("nlog.config");
+
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.ConfigureLoggerService();
+var app = builder.Build();
+
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseHsts();
+}
+
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
+app.UseAuthorization();
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"Logic before executing the next delegate in the Use method");
+    await next.Invoke();
+    Console.WriteLine("Logic after executing the next delegate in the Use method");
+
+
+});
+app.Map("/usingmapbranch", builder =>
+{
+    builder.Use(async (context, next) =>
+    {
+        Console.WriteLine("Logic before executing the next delegate in the Map method");
+        await next.Invoke();
+        Console.WriteLine("Logic after executing the next delegate in the Map method");
+    });
+    builder.Run(async context =>
+    {
+        Console.WriteLine("writing the response to the client in the Map method");
+        await context.Response.WriteAsync("Hello from the map branch.");
+    });
+});
+
+app.MapWhen(context => context.Request.Query.ContainsKey("testquerystring"), builder
+    =>
+{
+    builder.Run(async context =>
+    {
+
+        
+            await context.Response.WriteAsync("Hello from the MapWhen branch.");
+
+    }
+    );
+  
+});
+app.Run(async context =>
+{
+    Console.WriteLine("writing the response to the client in the run method");
+    await context.Response.WriteAsync("Hello from the middleware component.");
+});
+app.MapControllers();
+
+app.Run();
